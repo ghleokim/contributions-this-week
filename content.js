dbb78400a46e3f13c6002c4b0c245669081a8bcb
Leo Kim <gh.leokim@gmail.com>
2020-09-16 00:25:09 +0900

    Update comments, Delete unnecessary console log

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+/** Creates a div element with selected week's contributions. */
 function getWeekElement(element) {
   heading = getHeading()
   dateHeading = getDateHeading(element)
@@ -19,6 +20,7 @@ function getWeekElement(element) {
   document.body.appendChild(i);
 }
 
+/** Creates a title div for ctw. */
 function getHeading() {
   heading = document.createElement('div');
   heading.className = "ctw-heading"
@@ -26,6 +28,9 @@ function getHeading() {
   return heading
 }
 
+/** Creates a date sub header out of the element provided. 
+ *  for example : "2020-09-14 ~ 2020-09-20"
+ */
 function getDateHeading(element) {
   dHeading = document.createElement('div');
   dHeading.className = "ctw-subheading"
@@ -35,6 +40,7 @@ function getDateHeading(element) {
   return dHeading
 }
 
+/** Creates a table header div for the ctw table. */
 function getHeader() {
   headerBox = document.createElement('div');
   headerBox.className = "ctw-headerContainer"
@@ -52,6 +58,7 @@ function getHeader() {
   return headerBox
 }
 
+/** Creates the actual table content div for the ctw table. */
 function getWeek(element) {
   weekBox = document.createElement('div');
   weekBox.className = "ctw-weekContainer";
@@ -62,7 +69,7 @@ function getWeek(element) {
     dayBox.innerText = `-`
     weekBox.appendChild(dayBox)
   }
-  console.log(element)
+
   for (let e of element.__data__) {
     curDay = weekBox.children[e.day]
     curDay.innerText = e.count;
@@ -72,12 +79,19 @@ function getWeek(element) {
   return weekBox
 }
 
+
+/** Parses a <rect> element and parses data out of it
+ *  it can parse
+ *      <rect ... data-original-title="14 contributions<br />Friday Sep 11, 2020"></rect>
+ *  into
+ *      {count:14, day: 5, date: [Date object]}
+ */
 function parseTitle(element) {
   console.log(element)
   data = element.outerHTML.toString()
   data = data.split('"')
   if (data.length < 17) return;
-  
+
   data = data[17].split('>')
   contributionString = data[0].split(' ')[0]
   dateObj = new Date(data[1])
@@ -85,64 +99,44 @@ function parseTitle(element) {
   if (!Date.parse(dateObj)) return;
 
   return {
-      count: contributionString === 'No' ? 0 : contributionString,
-      day: dateObj.getDay(),
-      date: dateObj
+    count: contributionString === 'No' ? 0 : contributionString,
+    day: dateObj.getDay(),
+    date: dateObj
   }
 }
 
+/** A main function that gets data from HTML body. */
 async function run() {
-    svgs = document.querySelectorAll('svg')
-    console.log(svgs)
-
-    target = Array.from(svgs).filter(element => element.className.baseVal === "contrib-calendar")
-    target = target[0].children
-    targetTmp = ''
-    target = Array.from(target).map(function (el) {
-      domArray = new DOMParser().parseFromString(el.innerHTML.toString(), "text/html").body
-      console.log('aaaa', domArray, Array.from(domArray))
-
-      domArray = Array.from(domArray.children).map(el => parseTitle(el))
-      console.log(domArray)
-      return {__data__: domArray.filter(el => el)}
-    })
-
-    target.map(function (el) {
-      console.log(el)
-    })
-    console.log(target, targetTmp)
-
-    getWeekElement(target[target.length - 4])
-  }
-
-
-
-
-  console.log(document)
-  a = document.getElementById('js-overview')
-  a = a.children[0].children[0].children[0].children[0]
-
-  // Select the node that will be observed for mutations
-  const targetNode = a // document.getElementById('js-overview');
-
-  // Options for the observer (which mutations to observe)
-  const config = { attributes: true, childList: true, subtree: true };
-
-  // Callback function to execute when mutations are observed
-  const callback = function (mutationsList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for (let mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        console.log('A child node has been added or removed.');
-      }
-      else if (mutation.type === 'attributes') {
-        console.log('The ' + mutation.attributeName + ' attribute was modified.');
-      }
+  svgs = document.querySelectorAll('svg');
+  target = Array.from(svgs).filter(element => element.className.baseVal === "contrib-calendar");
+  target = target[0].children;
+  targetTmp = '';
+  target = Array.from(target).map(function (el) {
+    domArray = new DOMParser().parseFromString(el.innerHTML.toString(), "text/html").body;
+    domArray = Array.from(domArray.children).map(el => parseTitle(el));
+    return {
+      __data__: domArray.filter(el => el)
     }
-  };
+  })
 
-  // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(() => run().then(() => observer.disconnect()));
+  getWeekElement(target[target.length - 4]);
+}
 
-  // Start observing the target node for configured mutations
-  observer.observe(targetNode, config);
+jsOverview = document.getElementById('js-overview');
+jsOverview = jsOverview.children[0].children[0].children[0].children[0];
+
+// Select the node that will be observed for mutations
+const targetNode = jsOverview
+
+// Options for the observer (which mutations to observe)
+const config = {
+  attributes: true,
+  childList: true,
+  subtree: true
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(() => run().then(() => observer.disconnect()));
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);

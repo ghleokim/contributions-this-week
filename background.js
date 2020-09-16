d0480b0305750848d19b70934ddc1230a052ea68
Geonho Kim / Leo Kim <gh.leokim@gmail.com>
2020-09-16 11:07:18 +0900

    Update contentscript action tab openings

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
-chrome.tabs.executeScript({file: 'content.js'})
+chrome.tabs.onCreated.addListener(tab => {
+  const tabId = tab.id
+  console.log(tabId)
+  chrome.tabs.get(tabId, current_tab_info => {
+    console.log(`injected in ${tabId}`,current_tab_info)
+    if (/^https:\/\/lab\.ssafy\.com/.test(current_tab_info.pendingUrl)) {
+      chrome.tabs.executeScript(tabId, {file: "content.js"})
+      chrome.tabs.insertCSS(tabId, {file: "ctw.css"})
+    }
+  })
+})
